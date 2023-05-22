5378354c80442b5c42ca15648e549a1b8ba8c7df
Kwaku Boateng <kwakuboateng.dev@outlook.com>
2023-05-22 17:20:54 +0000

    server created

---
+++ server/index.js
@@ -0,0 +1,12 @@
+//server
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server listening on ${PORT}`);
+});
