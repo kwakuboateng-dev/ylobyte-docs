c1ae7b0850ed9ae6ade836fbe08139b188e104f1
Kwaku Boateng <kwakuboateng.dev@outlook.com>
2023-05-23 16:45:46 +0000

    connect to mongoDB

--- server/index.js
+++ server/index.js
@@ -7,6 +7,15 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+const DB ="mongodb+srv://ylobyte-admin:1Nc0dew3trust$$@ylobytecluster0.safxajc.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(DB).then(() => {
+  console.log("Connection successful!! 😊");
+})
+.catch((err) => {
+  console.log(err, " Connection failed. 😢");
+});
+
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server listening on ${PORT}`);
+  console.log(`http://localhost:${PORT}`);
 });
